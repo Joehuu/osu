d79dc0fbfa3d32a12294dfad195a99dee747cb5b
Joseph Madamba <35318437+Joehuu@users.noreply.github.com>
2018-02-23 10:50:00 -0800

    Update ModSuddenDeath.cs

--- osu.Game/Rulesets/Mods/ModSuddenDeath.cs
+++ osu.Game/Rulesets/Mods/ModSuddenDeath.cs
@@ -14,7 +14,7 @@ namespace osu.Game.Rulesets.Mods
         public override FontAwesome Icon => FontAwesome.fa_osu_mod_suddendeath;
         public override ModType Type => ModType.DifficultyIncrease;
         public override string Description => "Miss a note and fail.";
-        public override double ScoreMultiplier => 1.00;
+        public override double ScoreMultiplier => 1;
         public override bool Ranked => true;
         public override Type[] IncompatibleMods => new[] { typeof(ModNoFail), typeof(ModRelax), typeof(ModAutoPilot), typeof(ModAutoplay) };
 
