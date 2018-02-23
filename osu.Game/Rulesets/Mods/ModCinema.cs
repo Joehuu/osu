98b2fae79b9e82e6ebb5d3b28b32ceb06c16d410
Joseph Madamba <35318437+Joehuu@users.noreply.github.com>
2018-02-23 11:51:30 -0800

    Update ModCinema.cs

--- osu.Game/Rulesets/Mods/ModCinema.cs
+++ osu.Game/Rulesets/Mods/ModCinema.cs
@@ -11,9 +11,6 @@ namespace osu.Game.Rulesets.Mods
         public override string ShortenedName => "CN";
         public override FontAwesome Icon => FontAwesome.fa_osu_mod_cinema;
         public override string Description => "Watch video without visual distractions"; //add description
-        public override double ScoreMultiplier => 0
         public override bool HasImplementation => false;
-        public bool AllowFail => false;
-        public virtual bool Ranked => false;
     }
 }
