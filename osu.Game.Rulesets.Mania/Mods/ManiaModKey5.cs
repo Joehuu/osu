4df1aa78071a80acd86e9e75a0b64e34783bd62d
Joseph Madamba <35318437+Joehuu@users.noreply.github.com>
2018-02-23 11:42:35 -0800

    Update ManiaModKey5.cs

--- osu.Game.Rulesets.Mania/Mods/ManiaModKey5.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModKey5.cs
@@ -8,5 +8,7 @@ namespace osu.Game.Rulesets.Mania.Mods
         public override int KeyCount => 5;
         public override string Name => "Five Keys";
         public override string ShortenedName => "5K";
+        public override FontAwesome Icon => FontAwesome.fa_question;
+        public override string Description => "Play with five keys.";
     }
 }
